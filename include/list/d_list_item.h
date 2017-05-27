0c791e3076e48e0f7c4bacfb6efb55c3da5388ab
Sebastian Lindberg <seb.lindberg@gmail.com>
2017-05-27 09:30:55 +0200

    Added comments and cleaned up

--- include/list/d_list_item.h
+++ include/list/d_list_item.h
@@ -17,35 +17,61 @@ typedef struct d_list_item {
 
 /* Global Variables --------------------------------------------------------- */
 
+
+
 /* Public Functions --------------------------------------------------------- */
 
 void
   d_list_item__ctor(d_list_item_t *item);
 
-void
-  d_list_item__prepend(d_list_item_t *item, d_list_item_t *other);
-
+/* Insert a list item between `item` and the item directly following it. If
+ * `other` is connected to anything that whole chain will be inserted, with the
+ * last item linking to item after `item`.
+ */
 void
   d_list_item__append(d_list_item_t *item, d_list_item_t *other);
 
+/* Insert a list item between `item` and the item directly before it. If `other`
+ * is connected to anything that whole chain will be inserted, with the last
+ * item linking to `item`.
+ */
+void
+  d_list_item__prepend(d_list_item_t *item, d_list_item_t *other);
+
+/* Remove item from the chain it is connected to.
+ */
 void
   d_list_item__delete(d_list_item_t *item);
 
+/* Returns the item directly before `item`. If `item` is the first one in the
+ * chain NULL is returned.
+ */
 static inline d_list_item_t *
   d_list_item__prev(d_list_item_t const *item);
 
+/* Returns the item directly after `item`. If `item` is the last one in the
+ * chain NULL is returned.
+ */
 static inline d_list_item_t *
   d_list_item__next(d_list_item_t const *item);
 
+/* Returns non zero if `item` is the first one in the chain.
+ */
 static inline bool_t
   d_list_item__is_first(d_list_item_t const *item);
 
+/* Returns non zero if `item` is the last one in the chain.
+ */
 static inline bool_t
   d_list_item__is_last(d_list_item_t const *item);
-  
+
+/* Returns the first item in the chain connected to `item`.
+ */
 d_list_item_t *
   d_list_item__first(d_list_item_t const *item);
 
+/* Returns the last item in the chain connected to `item`.
+ */
 static inline d_list_item_t *
   d_list_item__last(d_list_item_t const *item);
 
@@ -64,27 +90,19 @@ d_list_item_t *d_list_item__next(d_list_item_t const *item)
   return (d_list_item_t *) s_list_item__next(&item->_super);
 }
 
-
-/* The first item has a reference to the last item, so check if the previous
- * item is the last.
- */
-
 bool_t d_list_item__is_first(d_list_item_t const *item)
 {
   assert(item != NULL);
+  /* The first item has a reference to the last item, so
+     check if the previous item is the last. */
   return d_list_item__is_last(d_list_item__prev(item));
 }
-
-
-/* All items but the last one points to the next item.
- */
 
 bool_t d_list_item__is_last(d_list_item_t const *item)
 {
   assert(item != NULL);
   return s_list_item__is_last(&item->_super);
 }
-
 
 d_list_item_t *d_list_item__last(d_list_item_t const *item)
 {
